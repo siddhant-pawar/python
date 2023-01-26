69ce848f886461757a44b95005cbe45b7bffb89c
siddhant-pawar <112252184+siddhant-pawar@users.noreply.github.com>
2023-01-27 01:40:18 +0530

    Add files via upload

---
+++ Python_Basic_Syntax-1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# Printing First Python Program
+a = "world!!!"
+b ="hello,\t"
+print(b, a)
+
+c = str(input("enter a string"))
+if c == a:
+  print("Five is greater than two!")#!/usr/bin/python3
+
+# Printing First Python Program
+a = "world!!!"
+b ="hello,\t"
+print(b, a)
+
+c = str(input("enter a string"))
+if c == a:
+  print("Five is greater than two!")
+else:
+    print(c)
+
+print("{} {} this is {}".format(b,a,c))
+
+
+
+
