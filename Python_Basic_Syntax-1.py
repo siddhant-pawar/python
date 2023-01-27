b2c49795e54c9798021154d0f217e210d487a91f
siddhant-pawar <112252184+siddhant-pawar@users.noreply.github.com>
2023-01-27 16:23:49 +0530

    Update Python_Basic_Syntax-1.py

--- Python_Basic_Syntax-1.py
+++ Python_Basic_Syntax-1.py
@@ -5,6 +5,23 @@ a = "world!!!"
 b ="hello,\t"
 print(b, a)
 
+#This is a comment.
+print("Hello, World!")
+
+#Multiline Comments with #hash.
+#This is a comment
+#type in
+#more than one line
+print("Hello, World!")
+
+#Multiline Comments
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
 c = str(input("enter a string"))
 if c == a:
   print("Five is greater than two!")#!/usr/bin/python3
