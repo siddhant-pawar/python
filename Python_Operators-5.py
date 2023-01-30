b911896c44cba5140059f550ad92fe93f4b96742
siddhant-pawar <112252184+siddhant-pawar@users.noreply.github.com>
2023-01-30 22:13:50 +0530

    Add files via upload

---
+++ Python_Operators-5.py
@@ -0,0 +1,81 @@
+'''
+#Write Python code with using operators:
+-Assignment operators
+-Arithmetic operators
+-Logical operators
+-Comparison operators
+-Bitwise operators
+-Membership operators
+'''
+
+####################################################################
+#Python - Assignment operators
+####################################################################
+
+a,b = 5,5
+a+=b
+print(a)
+a-=b
+print(b)
+a/=b
+print(a)
+a%=b
+print(a)
+a**=b
+print(a)
+
+
+
+####################################################################
+#Python - Arithmetic operators
+####################################################################
+
+a,b = 8,5
+print(a + b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+####################################################################
+#Python - Logical operators 
+####################################################################
+
+a,b = 8,5
+print(a + b)
+print(a-b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+####################################################################
+#Python - Comparison operators
+####################################################################
+
+a,b = 12,4
+print(a<b)
+print(a>b)
+print(a==b)
+print(a<=b)
+print(a>=b)
+print(a!=b)
+
+
+####################################################################
+#Python - Bitwise operators
+####################################################################
+
+a,b = 11,54
+print(a << b)
+print(a >> b)
+print(a&b)
+print(~a)
+print(a|b)
+
+####################################################################
+#Python - Membership operators
+####################################################################
+
+Memberlist = ["siddhant", "harish", "pratik", "sushil"]
+print("siddhant" in Memberlist)
+print("pratik" not in Memberlist)
